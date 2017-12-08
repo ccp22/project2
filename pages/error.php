72c891e065359a863ef6165b7636e0d685e59a1f
chandni chandrakant patel <ccp22@afsconnect1.njit.edu>
2017-12-07 23:06:28 -0500

    bootstrap sources applied

--- pages/error.php
+++ pages/error.php
@@ -9,6 +9,9 @@
     <meta name="author" content="SitePoint">
 
     <link rel="stylesheet" href="css/styles.css?v=1.0">
+    <?php
+        include 'meta.php';
+    ?>
 
     <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
